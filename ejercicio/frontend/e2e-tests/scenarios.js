f6a6c6729333937b850b6161a7cb340a6854fd81
Rodolfo Campos <camposer@gmail.com>
2015-11-25 17:50:06 +0100

    Prueba e2e

--- ejercicio/frontend/e2e-tests/scenarios.js
+++ ejercicio/frontend/e2e-tests/scenarios.js
@@ -2,41 +2,55 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('my app', function() {
+describe('Pruebas de aplicación', function() {
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  it('Cuando el usuario entra debe cargarse la ruta raíz', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).toMatch("/");
   });
 
 
-  describe('view1', function() {
+  describe('Módulo de productos', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('index.html#/producto');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+    it('Debería mostrar el título de producto', function() {
+      expect(element.all(by.css('h1')).first().getText()).
+        toMatch(/Productos/);
     });
 
+    it('Debería mostrar la lista de productos', function() {
+      expect(element.all(by.repeater('p in productos')).count()).toBeGreaterThan(0);
+    });
+
+    it('Debería agregar un producto', function() {
+      element(by.model('producto.nombre')).sendKeys('nuevo');
+      element(by.model('producto.precio')).sendKeys('200');
+
+      var tamanoInicial;
+
+      element
+        .all(by.repeater('p in productos'))
+        .count()
+        .then(function(valor) {
+          tamanoInicial = valor;
+        });
+
+      element(by.css('button[type=submit]')).click();
+          
+      element
+        .all(by.repeater('p in productos'))
+        .count()
+        .then(function(valor) {
+          expect(valor).toEqual(tamanoInicial + 1);
+        });
+
+    });
   });
 
 
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
